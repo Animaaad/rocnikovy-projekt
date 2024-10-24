2a35419a6b326c1c7ebf7b4e1c2edaeee3560ca4
Animaaad <151383438+Animaaad@users.noreply.github.com>
2024-10-24 18:09:22 +0200

    Add files via upload

---
+++ projekt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+int k = 6;
+int d = 3;
+int g = 6;
+
+int check_circuit(vector<vector<int>> graph) {
+    
+}
+int main() {
+    int 
+    cout << "" << endl;
+}
