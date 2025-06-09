fc7c312c1e133cf4668a377cf3e91e6b969c3953
Animaaad <151383438+Animaaad@users.noreply.github.com>
2025-06-09 15:12:36 +0200

    Add files via upload

--- projekt.cpp
+++ projekt.cpp
@@ -3,14 +3,15 @@
 #include <queue>
 #include <algorithm>
 #include <cmath>
+#include <fstream>
 int k;
 int d;
 int g;
 /*start is the vertex that is the start of the "outer layer",
 which is all the leaves of the k trees that
-are required to have minimal distance d
+are required to have minimal distance d between vertices that have 2 edges
 depth is the height of each individual tree
-outer_layer is the number of vertices that are on the "outer_layer"
+outer_layer is the number of vertices that are on the "outer layer"
 end is the last vertex in the graph
 odd_mode indicates if k is odd
 */
@@ -37,10 +38,12 @@ the first layer will have assigned the smallest numbers,
 the outer level will have assigned the biggest numbers.
 if k is even, then the trees will have d/2 layers, and
 we will only need to test edge connections between vertices on the outer layer
-if k is odd, then the trees will have also d/2 layers,
-however the outer layer won't be connected to anything initially and
-whether vertices connect to anything will have to be tested
-in the search function
+if k is odd, then the trees will have d/2 - 1 layers,
+and then we will add k * 2 / 3 vertices and
+0, 1 or 2 edges depending on the value (outer_layer * 2) % 3,
+because that value indicates how many edges need to exist before the start
+of the search as that is the minmal number of edges that need to exist
+for the graph with such parameters to exist
 */
 std::vector<std::vector<int>> initialize_graph() {
     std::vector<std::vector<int>> graf;
@@ -157,7 +160,7 @@ int check_cycle() {
 void prep() {
     for (int i = 0; i < k; i++) {
         std::vector<int> vec;
-        for (int j = start; j < tree_leaves + start; j++) {
+        for (int j = start; j < start + tree_leaves; j++) {
             vec.push_back(i * tree_leaves + j);
         }
         time_saving_vec.push_back(vec);
@@ -172,85 +175,53 @@ int tree_index(int value) {
     }
 }
 
+int parent(int j) {
+    return tree_index(j - outer_layer);
+}
+ 
 /*
-main function of the code
-the
+main function of the code for k odd
 @return whether the minimal possible graph for the parameters k, d, g exists
-if k is even, tests all the possible subsets of vertices of the complete graph
-on vertices {start, ..., end}, that are viable,
-i.e. no vertex has more than 3 edges incident with it, and
-no cycle is shorter than g
-if k is odd, tests all the possible subsets of vertices of the complete graph
-on vertices {odd_start, ..., end}, that are viable,
-i.e. no vertex has more than 3 edges incident with it, and
-no cycle is shorter than g*/
+*/
 bool search(int i, int j) {
-    if (i == j && j != end - 1)
-        return search(i, j + 1);
-    // std::cout<<i<<" j "<<j<<" "<<tree_index(i)<<" tj "<<tree_index(j)<<std::endl;
-    if (!odd_mode && abs(tree_index(i) - tree_index(j)) > 1 &&
-        !(tree_index(i) == 0 && tree_index(j) == k - 1)) {
-        if (j == end - 1) {
-            if (search(i + 1, start))
-                return true;
+    if (i == start + outer_layer) {
+        for (int i = k; i < end; i++) { // we can start from the variable start if k > 5
+            if (graph[i].size() != 3)
+                return false;
         }
-        else {
-            if (search(i, j + 1))
-                return true;
-        }
-        return false;
-    }
-    if (i == j) {
-        if (odd_mode) {
-            for (int i = k; i < end; i++) { // we can start from the variable start if k > 5
-                if (graph[i].size() != 3)
-                    return false;
-            }
-            for (int i = 0; i < k; i++) {
-                if (graph[i].size() != 2)
-                    return false; // only needed for k = 5
-            }
-        }
-        else {
-            for (int i = start; i < end; i++) {
-                if (graph[i].size() != 3)
-                    return false;
-            }
+        for (int i = 0; i < k; i++) {
+            if (graph[i].size() != 2)
+                return false; // only needed for k = 5
         }
         if (check_cycle() >= g)
             return true;
         return false;
     }
-    //std::cout << i << " j " << j << std::endl;
-    if ((j < i && !(j - i > outer_layer - (outer_layer / k * 2))) || 
-    (odd_mode && ((i < start + outer_layer && j < start + outer_layer) ||
-    (i >= start + outer_layer && j >= start + outer_layer)))) {
-        if (j == end - 1) {
-            if (search(i + 1, start)) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            if (search(i, j + 1)) {
-                return true;
-            }
-            else {
-                return false;
+    if (k % 3 == 0 && tree_index(i) < k * 2 / 3) {
+        if (parent(j) != tree_index(i) && parent(j) != tree_index(i) + 1) {
+            if (j == end - 1) {
+                if (search(i + 1, start + outer_layer)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if (search(i, j + 1)) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
             }
         }
-        return false;
     }
     if (!contains(graph[i], j) && graph[i].size() < 3 && graph[j].size() < 3) {
-        // std::cout<<i<<" "<<j<<std::endl;
         graph[i].push_back(j);
         graph[j].push_back(i);
-        // the cycle can appear only using the vertex i, so we can use bfs
+        //the cycle can appear only using the vertex i, so we can use bfs
         if (bfs(i) >= g) {
             if (j == end - 1) {
-                if (search(i + 1, start))
+                if (search(i + 1, start + outer_layer))
                     return true;
             }
             else {
@@ -264,7 +235,7 @@ bool search(int i, int j) {
         graph[j].erase(it);
     }
     if (j == end - 1) {
-        if (search(i + 1, start))
+        if (search(i + 1, start + outer_layer))
             return true;
     }
     else {
@@ -274,38 +245,29 @@ bool search(int i, int j) {
     return false;
 }
 
-void edge_case_function() {
-    for (int i = start; i < start + tree_leaves; i++) {
-        for (int j = start + tree_leaves; j < start + 2 * tree_leaves; j++) {
-            if (contains(graph[i], j)) {
-                auto it = std::find(graph[i].begin(), graph[i].end(), j);
-                graph[i].erase(it);
-                it = std::find(graph[j].begin(), graph[j].end(), i);
-                graph[j].erase(it);
-            }
-        }
-    }
-}
 
-bool edge_case_function2(int i, int j, int istart, int jstart) {
+bool edge_case_function(int i, int j, int istart, int jstart) {
     if (j == jstart + tree_leaves) {
-        //std::cout<<"god damn... "<< i << " "<< j <<std::endl;
         return false;
     }
     graph[i].push_back(j);
     graph[j].push_back(i);
-    // the cycle can appear only using the vertex i, so we can use bfs
     if (bfs(i) >= g) {
         if (i == istart + tree_leaves - 1) return true;
-        if (edge_case_function2(i + 1, jstart, istart, jstart)) return true;
+        if (edge_case_function(i + 1, jstart, istart, jstart)) return true;
     }
     auto it = std::find(graph[i].begin(), graph[i].end(), j);
     graph[i].erase(it);
     it = std::find(graph[j].begin(), graph[j].end(), i);
     graph[j].erase(it);
-    if (edge_case_function2(i, j + 1, istart, jstart)) return true;
+    if (edge_case_function(i, j + 1, istart, jstart)) return true;
     return false;
 }
+
+/*
+main function of the code for k odd
+@return whether the minimal possible graph for the parameters k, d, g exists
+*/
 
 bool search2() {
     int i = start, j = start;
@@ -328,13 +290,13 @@ bool search2() {
         i++;
         j = start;
     }
-    //edge_case_function();
-    edge_case_function2(start, end - tree_leaves, start, end - tree_leaves);
-    //edge_case_function2(start + tree_leaves, start, start + tree_leaves, start);
+    edge_case_function(start, end - tree_leaves, start, end - tree_leaves);
     return true;
 }
 
 int main() {
+    std::ofstream myfile;
+    myfile.open("vystup.txt");
     std::cin >> k >> g >> d;
     depth = d / 2;
     start = k * (std::pow(2, depth) - 1);
@@ -342,44 +304,34 @@ int main() {
     end = start + outer_layer;
     n = end;
     tree_leaves = outer_layer / k;
-    prep();
     if (k % 2 == 1) {
-        depth--;
         odd_mode = true;
+        depth--;
+        tree_leaves /= 2;
         outer_layer = (std::pow(2, (depth))) * k;
         start = k * (std::pow(2, depth) - 1);
         extra_start = start + outer_layer;
-        extra = (outer_layer * 2 + 3) / 3;
+        extra = (outer_layer * 2) / 3;
+        if (outer_layer % 3 != 0) extra++;
         end = start + outer_layer + extra;
         n = end;
     }
+    prep();
     graph = initialize_graph();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < graph[i].size(); j++) {
-            // std::cout << i << ' ' << graph[i][j] << " g" << std::endl;
-        }
-    }
-    for (int i = 0; i < k; i++) {
-        for (int j = 0; j < time_saving_vec[i].size(); j++) {
-            // std::cout << i << ' ' << time_saving_vec[i][j] << " t" << std::endl;
-        }
-    }
     int a = 0;
     if (odd_mode) {
-        a = search(start, start);
+        a = search(start, start + outer_layer);
     }
     else {
         a = search2();
     }
-
     if (a == 0) std::cout << "false" << std::endl;
-    /*will print the graph that is compatible with our criteria
-    if such graph exists*/
+    //will print the graph that is compatible with our criteria
     else {  
         std::cout << "true" << std::endl;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < graph[i].size(); j++) {
-                //std::cout << i << ' ' << graph[i][j] << std::endl;
+                myfile << i << ' ' << graph[i][j] << std::endl;
                 if ((!odd_mode && graph[i].size() != 3 && i > start)) {
                     std::cout <<"wrong edges..." << i << " " << tree_index(i) << " " << j << std::endl;
                 }
